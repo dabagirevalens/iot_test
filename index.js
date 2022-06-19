335215aa7202a02a2b0a5e9f11a929a4224d163d
dabagirevalens <vdabagire@gmail.com>
2022-06-19 19:05:22 +0200

    added cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 
 const Content = require('./content.model');
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
 
 
 //connect to DB
